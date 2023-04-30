67a5febf82e9c4026ce52eb94568a959bb175c04
hanamuratukito <keikennouta11@gmail.com>
2023-04-30 17:10:12 +0900

    create：ヘッダーコンポーネント作成

--- README.md
+++ README.md
@@ -24,3 +24,17 @@ error Found incompatible module.
 ２．https://qiita.com/Co-0/items/db8d36d31b1480b92d64
 
 ３．https://rails-ambassador.herokuapp.com/debugs/NoDatabaseError
+
+
+# 導入ライブラリ
+
+１．以下より、redux導入
+https://qiita.com/keitakn/items/7433c89ce52073e861a1#%E3%83%87%E3%82%A3%E3%83%AC%E3%82%AF%E3%83%88%E3%83%AA%E6%A7%8B%E6%88%90%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6
+
+# フォルダ構成
+
+https://note.com/ryoppei/n/n2e3e7a66e758
+
+# マテリアルUIの導入
+
+https://qiita.com/tmgauss/items/07d04d78c6df59ba2fb6
--- frontend/app/pages/_app.tsx
+++ frontend/app/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import Header from '../components/layouts/header/Header';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+  return (
+    // 「JSX expressions must have one parent element.」対策に<>を導入
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
