d1ec5e9005505f8a97a4f1c5da9e7ae7732cdf08
hanamuratukito <keikennouta11@gmail.com>
2023-05-28 13:35:44 +0900

    add：メール情報をDBに登録

--- backend/app/controllers/api/gmail/gmail_controller.rb
+++ backend/app/controllers/api/gmail/gmail_controller.rb
@@ -41,6 +41,7 @@ class Api::Gmail::GmailController < ApplicationController
   end
 
   def get_mail
+    # 未読の件名がWF or 見積書のメールを取得
     token = session[:google_access_token]
     uri = URI.parse(escape("https://www.googleapis.com/gmail/v1/users/me/messages?q=subject:{WF 見積書} AND is:unread"))
     request = Net::HTTP::Get.new(uri)
@@ -49,8 +50,14 @@ class Api::Gmail::GmailController < ApplicationController
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-
     jsonRes = JSON.parse(response.body)
+
+    # 該当するメールが無い場合は処理終了
+    if jsonRes.blank?
+      render json: { result_status: 1000 }
+    end
+
+    # 取得したメールをもとにDBに登録する
     gmails = []
     for msg in jsonRes['messages']
       gmailUri = URI.parse(escape("https://www.googleapis.com/gmail/v1/users/me/messages/#{msg['id']}"))
@@ -59,9 +66,42 @@ class Api::Gmail::GmailController < ApplicationController
       gmail = Net::HTTP.start(gmailUri.hostname, gmailUri.port, req_options) do |http|
         http.request(gmailRequest)
       end
-      gmails.push(JSON.parse(gmail.body))
+      gmail_info = JSON.parse(gmail.body)
+
+      # mail_infosテーブルへの登録
+      mail_info_table = MailInfo.new
+      request_table = Request.new
+      for header in gmail_info['payload']['headers']
+        if header['name'] === 'Subject' then
+          mail_info_table.subject = header['value']
+
+          if header['value'].include?("WF") then
+            request_table.business_type = 1
+          else
+            request_table.business_type = 2
+          end
+
+        elsif header['name'] === 'To' then
+          mail_info_table.to = header['value']
+        elsif header['name'] === 'From' then
+          mail_info_table.from = header['value']
+        end
+      end
+
+      # TODO 添付ファイルは別途対応
+      mail_info_table.body = gmail_info['snippet']
+      request_table.request_name = gmail_info['snippet']
+      request_table.status = 0
+      request_table.contact = 'システムユーザー'
+      request_table.created_user_id = 1
+      request_table.updated_user_id = 1
+      mail_info_table.save
+      request_table.mail_info_id = mail_info_table.id
+      request_table.save
+      # gmails.push(JSON.parse(gmail.body))
     end
-    render json: { gmails: gmails, res: jsonRes }
+    render json: { result_status: 0000 }
+    # render json: { gmails: gmails, res: jsonRes }
   end
 
   def escape(str)
