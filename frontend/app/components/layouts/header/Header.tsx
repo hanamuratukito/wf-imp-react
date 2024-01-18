c9f1a92f48ee72aca60e21efe00f90fea0dcd152
keikenn <keikennouta11@gmail.com>
2024-01-18 17:44:01 +0900

    update：npmコマンドによる更新

--- frontend/app/components/layouts/header/Header.tsx
+++ frontend/app/components/layouts/header/Header.tsx
@@ -7,9 +7,16 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useEffect } from 'react';
 import { useHeader } from '../../../hooks/useHeader';
+import { styled } from '@mui/material/styles';
+
+const CustomIcon = styled(IconButton)(({ theme }) => ({
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+}));
 
 export default function ButtonAppBar() {
-  const { classes, isLogin, isClient, tryLogout, setIsClient } =
+  const { isLogin, isClient, tryLogout, setIsClient } =
     useHeader();
 
   useEffect(() => {
@@ -29,14 +36,13 @@ export default function ButtonAppBar() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar className="flex">
-        <IconButton
+        <CustomIcon
           edge="start"
-          className={classes.menuButton}
           color="inherit"
           aria-label="menu"
         >
           <MenuIcon />
-        </IconButton>
+        </CustomIcon>
         <Typography variant="h6">ログイン</Typography>
         {logoutButton()}
       </Toolbar>
--- frontend/app/pages/_document.tsx
+++ frontend/app/pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@mui/material/styles';
 
 class MyDocument extends Document {
   render() {
@@ -17,13 +16,7 @@ class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
 
   const initialProps = await Document.getInitialProps(ctx);
 
@@ -31,7 +24,6 @@ MyDocument.getInitialProps = async (ctx) => {
     ...initialProps,
     styles: [
       ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
     ],
   };
 };
